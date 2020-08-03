3433d630a0b19b85cd101d955254ac9d69a08988
Pravin-Parkhi <pravinparkhi.pp@gmail.com>
2020-08-04 00:39:23 +0530

    integrate api for fetcing user records

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 function findPerson(){
-    window.location = "http://127.0.0.1:50071/search-details.html";
+    debugger
+    const serchInput = document.getElementById('searchInput');
+    const email = serchInput.value;
+//    window.location = `http://127.0.0.1:50071/search-details.html?email=${email}`;
+//    window.replace = `http://127.0.0.1:50071/search-details.html`
+}
+
+function fetchRecords(){
+    const urlParams = new URLSearchParams(window.location.search);
+    const email = urlParams.get('email');
+    debugger
+    const url = `https://ltv-data-api.herokuapp.com/api/v1/records.json?email=${email}`
+    fetch(url)
+        .then(function(data) {
+        debugger
+        console.log(data)
+    })
+        .catch(function(error) {
+        // If there is any error you will catch them here
+    });
 }
